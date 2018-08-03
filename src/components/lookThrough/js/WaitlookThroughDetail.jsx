fd4d59de87b5bd34c159e62e8091f8350cab442a
zhaojuan <343524032@qq.com>
2018-08-03 18:33:06 +0800

    -

--- src/components/lookThrough/js/WaitlookThroughDetail.jsx
+++ src/components/lookThrough/js/WaitlookThroughDetail.jsx
@@ -130,54 +130,61 @@ export default class WaitlookThroughDetail extends React.Component {
             <div id="waitLookThrough" style={{
                 height: document.body.clientHeight
             }}>
-                {
-                    calm.state.type == 0 ?
-                        <div className="sameBack">
-                            <img style={{width:"50px",height:"50px"}} src={calm.state.data.userInfo ? calm.state.data.userInfo.avatar : ""} alt=""/>
-                            <div>类型：自媒体文章</div>
-                            <div>标题：{calm.state.data.articleTitle}</div>
-                            <div>作者：{calm.state.data.userInfo ? calm.state.data.userInfo.userName : ""}</div>
-                            <div>上传时间：{WebServiceUtil.formatYMD(calm.state.data.createTime)}</div>
-                            <div>内容：{calm.state.data.articleContent}</div>
-                        </div>
-                        :
-                        calm.state.type == 1 ?
+                <div className="content">
+                    {
+                        calm.state.type == 0 ?
                             <div className="sameBack">
-                                <img style={{width:"50px",height:"50px"}} src={calm.state.data.userInfo ? calm.state.data.userInfo.avatar:""} alt=""/>
-                                <div>类型：短视频</div>
-                                <div>作者：{calm.state.data.userInfo ? calm.state.data.userInfo.userName : ""}</div>
-                                <div>上传时间：{WebServiceUtil.formatYMD(calm.state.data.createTime)}</div>
-                                <div>内容：
-                                    <video 
-                                        controls="controls" 
-                                        preload="auto"  
-                                        style={{objectFit: "fill",width:"100%"}}
-                                        src={calm.state.data.videoPath}>
-                                    </video>
+                                <div className='title'>标题：{calm.state.data.articleTitle}</div>
+                                <div className='topMsg'>
+                                    <img className="photo" src={calm.state.data.userInfo ? calm.state.data.userInfo.avatar : ""} alt=""/>
+                                    <span className='author'>{calm.state.data.userInfo ? calm.state.data.userInfo.userName : ""}</span>
+                                    <span className='time'>{WebServiceUtil.formatYMD(calm.state.data.createTime)}</span>
+                                    <span className="type">{/*类型：自媒体文章*/}<img src={require("../img/icon_media.png")}/></span>
                                 </div>
+                                <div className='textCont'>{calm.state.data.articleContent}</div>
                             </div>
                             :
-                            ""
-                }
-                <div className="isDangerArea">
-                    <List renderHeader={() => '审核：'}>
-                        {data2.map(i => (
-                            <RadioItem key={i.value} checked={isPass === i.value} onChange={() => this.redioChange(i.value)}>
-                                {i.label}<List.Item.Brief>{i.extra}</List.Item.Brief>
-                            </RadioItem>
-                        ))}
+                            calm.state.type == 1 ?
+                                <div className="sameBack">
+                                     <div className='topMsg'>
+                                        <img className="photo" src={calm.state.data.userInfo ? calm.state.data.userInfo.avatar:""} alt=""/>
+                                        <span className='author'>{calm.state.data.userInfo ? calm.state.data.userInfo.userName : ""}</span>
+                                        <span className='time'>{WebServiceUtil.formatYMD(calm.state.data.createTime)}</span>
+                                        <span className="type">{/*类型：短视频*/}<img src={require("../img/icon_video.png")}/></span>
+                                     </div>
+                                     <div className="textCont">
+                                        <video
+                                            controls="controls"
+                                            preload="auto"
+                                            style={{objectFit: "fill",width:"100%"}}
+                                            src={calm.state.data.videoPath}>
+                                        </video>
+                                    </div>
+                                </div>
+                                :
+                                ""
+                    }
+                    <div className="isDangerArea">
+                        <List renderHeader={() => '审核：'}>
+                            {data2.map(i => (
+                                <RadioItem key={i.value} checked={isPass === i.value} onChange={() => this.redioChange(i.value)}>
+                                    {i.label}
+                                    {/*<List.Item.Brief>{i.extra}</List.Item.Brief>*/}
+                                </RadioItem>
+                            ))}
+                        </List>
+                    </div>
+                    <div className="sameBack description">审核说明:
+                    <List>
+                            <TextareaItem
+                                rows={3}
+                                placeholder="请在此处输入审核的说明／不通过的原因"
+                                onChange={v => _this.setState({
+                                    textareaValue: v
+                                })}
+                            />
                     </List>
-                </div>
-                <div className="sameBack">审核说明:
-                <List>
-                        <TextareaItem
-                            rows={3}
-                            placeholder="请在此处输入审核的说明／不通过的原因"
-                            onChange={v => _this.setState({
-                                textareaValue: v
-                            })}
-                        />
-                </List>
+                    </div>
                 </div>
                 <div className="submitBtn">
                     <Button type='warning' onClick={_this.submit}>提交</Button>
--- src/components/lookThrough/js/lookThrough.jsx
+++ src/components/lookThrough/js/lookThrough.jsx
@@ -133,26 +133,25 @@ export default class lookThrough extends React.Component {
                                 <div className="line_public">
                                     {
                                         v.littleVideoInfo ?
-                                            <div className="item" onClick={_this.toWaitLookThrough.bind(this, v.littleVideoInfoID, v.type)}>
-                                                <img style={{width:"50px",height:"50px"}} src={v.littleVideoInfo ? v.littleVideoInfo.userInfo.avatar : ""} alt=""/>
-                                                <span>类型：短视频</span>
-                                                
-                                                <div>
-                                                    <div>作者：{v.littleVideoInfo.userInfo.userName}</div>
-                                                    <div>上传时间：{WebServiceUtil.formatYMD(v.littleVideoInfo.createTime)}</div>
+                                            <div className="item my_flex" onClick={_this.toWaitLookThrough.bind(this, v.littleVideoInfoID, v.type)}>
+                                                <img className='photo' src={v.littleVideoInfo ? v.littleVideoInfo.userInfo.avatar : ""} alt=""/>
+                                                <div className='right'>
+                                                    <span className='author'>{v.littleVideoInfo.userInfo.userName}</span>
+                                                    <span className="type">{/*类型：短视频*/} <img src={require("../img/icon_video.png")}/></span>
+                                                    <div className='time'>{WebServiceUtil.formatYMD(v.littleVideoInfo.createTime)}</div>
                                                 </div>
                                             </div>
                                             :
                                             v.articleInfo ?
-                                                <div className="item" onClick={_this.toWaitLookThrough.bind(this, v.articleInfoId, v.type)}>
-                                                    <img style={{width:"50px",height:"50px"}} src={v.articleInfo ? v.articleInfo.userInfo.avatar : ""} alt=""/>
-                                                    
-                                                    <span>类型：自媒体文章</span>
-                                                    <div>
-                                                        <div>标题：{v.articleInfo.articleTitle}</div>
-                                                        <div>作者：{v.articleInfo.userInfo ? v.articleInfo.userInfo.userName : ""}</div>
-                                                        <div>上传时间：{WebServiceUtil.formatYMD(v.articleInfo.createTime)}</div>
+                                                <div className="item my_flex" onClick={_this.toWaitLookThrough.bind(this, v.articleInfoId, v.type)}>
+                                                    <img className='photo' src={v.articleInfo ? v.articleInfo.userInfo.avatar : ""} alt=""/>
+                                                    <div className='right'>
+                                                        <span className='author'>{v.articleInfo.userInfo ? v.articleInfo.userInfo.userName : ""}</span>
+                                                        <span className="type">{/*类型：自媒体文章*/}<img src={require("../img/icon_media.png")}/></span>
+                                                        <div className='title'>{v.articleInfo.articleTitle}</div>
+                                                        <div className='time'>{WebServiceUtil.formatYMD(v.articleInfo.createTime)}</div>
                                                     </div>
+
                                                 </div>
                                                 :
                                                 ""
@@ -174,24 +173,24 @@ export default class lookThrough extends React.Component {
                                 <div className="line_public">
                                     {
                                         v.littleVideoInfo ?
-                                            <div className="item" onClick={_this.toAlreadyLookThrough.bind(this, v.littleVideoInfoID, v.type, v.auditId)}>
-                                                <img style={{width:"50px",height:"50px"}} src={v.littleVideoInfo ? v.littleVideoInfo.userInfo.avatar : ""} alt=""/>
-                                                <span>类型：短视频</span>
-                                                <div>
-                                                    <div>作者：{v.littleVideoInfo.userInfo.userName}</div>
-                                                    <div>上传时间：{WebServiceUtil.formatYMD(v.littleVideoInfo.createTime)}</div>
+                                            <div className="item my_flex" onClick={_this.toAlreadyLookThrough.bind(this, v.littleVideoInfoID, v.type, v.auditId)}>
+                                                <img className='photo' src={v.littleVideoInfo ? v.littleVideoInfo.userInfo.avatar : ""} alt=""/>
+                                                <div className="right">
+                                                    <span className='author'>{v.littleVideoInfo.userInfo.userName}</span>
+                                                    <span className='type'>{/*类型：短视频*/}<img src={require("../img/icon_video.png")}/></span>
+                                                    <div className='time'>{WebServiceUtil.formatYMD(v.littleVideoInfo.createTime)}</div>
                                                 </div>
+
                                             </div>
                                             :
                                             v.articleInfo ?
-                                                <div className="item" onClick={_this.toAlreadyLookThrough.bind(this, v.articleInfoId,v.type, v.auditId)}>
-                                                    <img style={{width:"50px",height:"50px"}} src={v.articleInfo ? v.articleInfo.userInfo.avatar : ""} alt=""/>
-                                                    
-                                                    <span>类型：自媒体文章</span>
-                                                    <div>
-                                                        <div>标题：{v.articleInfo.articleTitle}</div>
-                                                        <div>作者：{v.articleInfo.userInfo ? v.articleInfo.userInfo.userName : ""}</div>
-                                                        <div>上传时间：{WebServiceUtil.formatYMD(v.articleInfo.createTime)}</div>
+                                                <div className="item my_flex" onClick={_this.toAlreadyLookThrough.bind(this, v.articleInfoId,v.type, v.auditId)}>
+                                                    <img className='photo' src={v.articleInfo ? v.articleInfo.userInfo.avatar : ""} alt=""/>
+                                                    <div className="right">
+                                                        <span className='author'>{v.articleInfo.userInfo ? v.articleInfo.userInfo.userName : ""}</span>
+                                                        <span  className='type'>{/*类型：自媒体文章*/}<img src={require("../img/icon_media.png")}/></span>
+                                                        <div className='title'>题：{v.articleInfo.articleTitle}</div>
+                                                        <div className='time'>上传时间：{WebServiceUtil.formatYMD(v.articleInfo.createTime)}</div>
                                                     </div>
                                                 </div>
 

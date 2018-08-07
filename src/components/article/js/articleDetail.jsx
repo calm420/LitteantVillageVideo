e34973c83553bc03ac2fd287a28a56cbcda2a02d
songyongfeng <446942919@qq.com>
2018-08-07 15:05:07 +0800

    .

--- src/components/article/js/articleDetail.jsx
+++ src/components/article/js/articleDetail.jsx
@@ -295,7 +295,7 @@ export default class articleDetail extends React.Component {
                     <div className="p15">
                         <div className="title">{this.state.data.articleTitle}</div>
                         <div className="at">
-                            <div className="author">{this.state.data.userInfo.userName}</div>
+                            <div className="author">{this.state.data.articleTitle}</div>
                             <div className="createTime">{WebServiceUtil.formatYMD(this.state.data.createTime)}</div>
                         </div>
                         <div className="content" dangerouslySetInnerHTML={{__html:this.state.data.articleContent}}></div>

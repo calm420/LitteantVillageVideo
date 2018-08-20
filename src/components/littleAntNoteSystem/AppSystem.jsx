62410854e162f8621c436aa201697dd6c9528297
songyongfeng <446942919@qq.com>
2018-08-20 18:50:50 +0800

    .

--- src/components/littleAntNoteSystem/AppSystem.jsx
+++ src/components/littleAntNoteSystem/AppSystem.jsx
@@ -5,15 +5,18 @@ import {Drawer, NavBar, Radio, Toast, Modal} from 'antd-mobile';
 import './AppSystem.less'
 import List from './leftComponent/js/List'
 import ReadPanel from './rightComponent/js/ReadPanel'
+var _this;
 const alert = Modal.alert;
 export default class AppSystem extends React.Component {
     constructor(props) {
         super(props);
+        _this = this;
         this.state = {
             title: 'app',
             open: false,
             user: JSON.parse(sessionStorage.getItem("loginUser")),
             clientHeight: document.body.clientHeight,
+            maskFlag:false,
         };
     }
 
@@ -21,10 +24,10 @@ export default class AppSystem extends React.Component {
         //mobile项目全局禁用原生下拉刷新
         Bridge.setRefreshAble("false");
         console.log(JSON.parse(sessionStorage.getItem("loginUser")))
-        if(this.state.user){
+        if (this.state.user) {
 
-        }else {
-            location.hash="Login";
+        } else {
+            location.hash = "Login";
         }
 
     }
@@ -53,16 +56,35 @@ export default class AppSystem extends React.Component {
         }
         var _this = this;
         const alertInstance = alert('您确定退出登录吗?', '', [
-            { text: '取消', onPress: () => console.log('cancel'), style: 'default' },
-            { text: '确定', onPress: () => _this.exit() },
+            {text: '取消', onPress: () => console.log('cancel'), style: 'default'},
+            {text: '确定', onPress: () => _this.exit()},
 
         ], phone);
     }
 
-    exit(){
+    exit() {
         sessionStorage.removeItem("loginUser");
-        location.hash="Login"
+        location.hash = "Login"
     }
+
+    setPanel(key,html) {
+        console.log(key);
+        console.log(html);
+        if(key == 'openPrieview'){
+            _this.setState({
+                maskFlag: true,
+                label: html,
+            })
+        }
+        console.log(_this.state.maskFlag)
+    }
+
+    closeMask(){
+        _this.setState({
+            maskFlag: false
+        })
+    }
+
     render() {
         var _this = this;
         return (
@@ -76,21 +98,27 @@ export default class AppSystem extends React.Component {
                         <img className="headPic" src={this.state.user.avatar} alt=""/>,
                         <div className="userName">{this.state.user.userName}</div>,
                         <div className="user_line"></div>,
-                        <div className="cancellation"><i className="user_exit"></i><span onClick={_this.showAlert}>退出</span></div>,
+                        <div className="cancellation"><i className="user_exit"></i><span
+                            onClick={_this.showAlert}>退出</span></div>,
                     ]}
                 >
                     {/*<div className="logo">校园自媒体</div>*/}
                     {/*<div className="userInfo">*/}
-                        {/*<div className="headPic_box">*/}
-                            {/*<img className="headPic" src={this.state.user.pic} alt=""/>*/}
-                        {/*</div>*/}
-                        {/*<div className="userName">{this.state.user.name}</div>*/}
-                        {/*<div className="cancellation">退出</div>*/}
+                    {/*<div className="headPic_box">*/}
+                    {/*<img className="headPic" src={this.state.user.pic} alt=""/>*/}
+                    {/*</div>*/}
+                    {/*<div className="userName">{this.state.user.name}</div>*/}
+                    {/*<div className="cancellation">退出</div>*/}
                     {/*</div>*/}
                 </NavBar>
-                <div class="black_bg" style={
-                    {display:'none'}
-                }></div>
+                <div className="black_bg" style={
+                   this.state.maskFlag? {display: 'inline-block'}:{display:'none'}
+                } onClick={this.closeMask}></div>
+                <div className="preview" style={
+                    this.state.maskFlag? {display: 'inline-block'}:{display:'none'}
+                } dangerouslySetInnerHTML={{
+                    __html: this.state.label
+                }}></div>
                 <div id='main'>
                     <div className="left">
                         <List
@@ -102,6 +130,7 @@ export default class AppSystem extends React.Component {
                         <ReadPanel
                             submit={this.submit}
                             ref='listToReadPanel'
+                            setPanel={this.setPanel}
                         />
                     </div>
                 </div>

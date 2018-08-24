c6b5f37eaf616a06729f4da553716a7ec914cbc0
noom <noom0130@foxmail.com>
2018-08-24 15:11:48 +0800

    .

--- src/components/article/js/articleList.jsx
+++ src/components/article/js/articleList.jsx
@@ -66,7 +66,6 @@ export default class articleList extends React.Component {
                     initLoading: false,
                 })
             })
-
         })
         this.refurbishNoom()
     }

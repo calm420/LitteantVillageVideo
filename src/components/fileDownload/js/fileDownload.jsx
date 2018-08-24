f99ef7b0fd6f1103a615a83c1c72b6f6f771c6fb
noom <noom0130@foxmail.com>
2018-08-24 16:50:15 +0800

    .

--- src/components/fileDownload/js/fileDownload.jsx
+++ src/components/fileDownload/js/fileDownload.jsx
@@ -47,15 +47,48 @@ export default class Demo extends React.Component {
                 <div>科技改变未来,教育成就未来</div>
             </div>
         } else if (fileType == 'elearning') {
-
+            var titleDiv = <div>
+                <div>小蚂蚁.云校</div>
+                <div>科技改变未来,教育成就未来</div>
+            </div>
         }
         this.setState({titleDiv})
     }
 
     downLoadFile = () => {
-        console.log(this.state.fileType);
-        console.log(this.state.phone);
-        // window.open('http://60.205.86.217/upload7_app/2018-08-23/20/5e2c5613-5a3d-48ce-8152-8fc64cef03b0.apk')
+        var fileType = this.state.fileType;
+        var phone = this.state.phone
+        if (fileType == 'youyang') {
+            if (phone == 'IOS') {
+                window.open('https://itunes.apple.com/cn/app/apple-store/id1423189213?mt=8')
+            } else {
+                window.open('http://60.205.86.217/upload7_app/2018-08-23/20/5e2c5613-5a3d-48ce-8152-8fc64cef03b0.apk')
+            }
+        } else if (fileType == 'littleAntTe') {
+            if (phone == 'IOS') {
+                window.open('https://itunes.apple.com/cn/app/apple-store/id1049156218?mt=8')
+            } else {
+                window.open('http://60.205.86.217/upload7_app/2018-07-16/19/4e303b59-f115-49e9-9705-d38b5f649c52.apk')
+            }
+        } else if (fileType == 'littleAntSt') {
+            if (phone == 'IOS') {
+                window.open('https://itunes.apple.com/cn/app/apple-store/id1049156218?mt=8')
+            } else {
+                window.open('http://60.205.86.217/upload7_app/2018-07-19/9/93d48c33-cda5-4784-97f9-9f132bbc63e8.apk')
+            }
+        } else if (fileType == 'littleAntFa') {
+            if (phone == 'IOS') {
+                window.open('https://itunes.apple.com/cn/app/apple-store/id1395849263?mt=8')
+            } else {
+                window.open('http://60.205.86.217/upload7_app/2018-06-28/21/f500639a-e8e5-43e2-b813-be6ffbf2f10a.apk')
+            }
+        } else if (fileType == 'elearning') {
+            if (phone == 'IOS') {
+                window.open('https://itunes.apple.com/cn/app/apple-store/id1268534857?mt=8')
+            } else {
+                window.open('http://60.205.86.217/upload7_app/2018-08-02/19/3c09e2df-fd58-4f81-8b5f-7deb38748000.apk')
+            }
+        }
     }
 
     render() {

1278b592af2ee09227623497067ca7acf7938e5c
luble <282558417@qq.com>
2018-08-31 20:02:41 +0800

    -

--- src/components/mobileEditor/js/mobileEditor.jsx
+++ src/components/mobileEditor/js/mobileEditor.jsx
@@ -84,7 +84,7 @@ export default class mobileEditor extends React.Component {
     render() {
         return (
             <div id="mobileEditor">
-                <iframe id="iframe_mobile" src="https://192.168.50.163:6443/mobileEditor/" frameborder="0"></iframe>
+                <iframe id="iframe_mobile" className="mobile-iframe" src="https://192.168.50.163:6443/mobileEditor/" frameborder="0"></iframe>
             </div>
 
         )

2fd570a4fab15bbe80752729191dbf0151b46d17
noom <noom0130@foxmail.com>
2018-08-31 23:47:48 +0800

    .

--- src/helpers/webServiceUtil.js
+++ src/helpers/webServiceUtil.js
@@ -4,7 +4,6 @@ var isDebugLocal = false;
 var localUrl = "192.168.50.29";    //跳转地址http:
 
 
-
 // //云校本地测试webService地址
 // var elearningWebserviceURLOfLocal = "http://" + localDomain + ":8888/elearning/elearningControl/";
 // //云校的远程服务器地址

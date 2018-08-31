0ceb830113c7d078f64c85c7e00420f28132f92a
noom <noom0130@foxmail.com>
2018-08-31 18:29:47 +0800

    .

--- src/components/mobileEditor/js/mobileEditor.jsx
+++ src/components/mobileEditor/js/mobileEditor.jsx
@@ -1,34 +1,54 @@
 import React from "react";
-import { List, Radio, TextareaItem, Toast, Modal, Button, Icon } from 'antd-mobile';
+import {} from 'antd-mobile';
 import "../css/mobileEditor.less"
-const RadioItem = Radio.RadioItem;
-const alert = Modal.alert;
-var calm;
 
+var _this;
 export default class mobileEditor extends React.Component {
     constructor(props) {
         super(props);
-        calm = this;
-        this.state = {
-
-        }
+        _this = this;
+        this.state = {}
     }
+
     componentDidMount() {
         document.title = "手机编辑器";
         var locationHref = window.location.href;
         var locationSearch = locationHref.substr(locationHref.indexOf("?") + 1);
         var searchArray = locationSearch.split("&");
         var id = searchArray[0].split('=')[1];
-        console.log('mobileEditor')
+        setTimeout(function () {
+            _this.getLittleVideoUserById(id)
+        }, 100)
     }
 
+    getLittleVideoUserById(id) {
+        var param = {
+            "method": 'getLittleVideoUserById',
+            "uid": id,
+        };
+        WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
+            onResponse: result => {
+                if (result.success) {
 
+                    var data = {}
+                    data.method = 'loginUserObj';
+                    data.mes = result.response
+
+                    var ifm = document.getElementById('iframe_mobile');
+                    ifm.contentWindow.postMessage(JSON.stringify(data), '*');
+                }
+            },
+            onError: function (error) {
+                // Toast.fail(error, 1);
+            }
+        });
+    }
 
 
     render() {
         return (
             <div id="mobileEditor">
-                <iframe id="iframe_mobile" src="https://192.168.50.186:6443/mobileEditor/" frameborder="0"></iframe>
+                <iframe id="iframe_mobile" src="https://192.168.50.29:6443/mobileEditor/" frameborder="0"></iframe>
             </div>
 
         )

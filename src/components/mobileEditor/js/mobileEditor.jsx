9503b258293aca62237c506b1186ddb8692e881a
noom <noom0130@foxmail.com>
2018-08-31 22:34:35 +0800

    .

--- src/components/mobileEditor/js/mobileEditor.jsx
+++ src/components/mobileEditor/js/mobileEditor.jsx
@@ -7,7 +7,9 @@ export default class mobileEditor extends React.Component {
     constructor(props) {
         super(props);
         _this = this;
-        this.state = {}
+        this.state = {
+            loginUser: {}
+        }
     }
 
     componentDidMount() {
@@ -30,7 +32,7 @@ export default class mobileEditor extends React.Component {
             var iframeData = JSON.parse(e.data);
             if (iframeData.method == 'mobile-submit') {
                 //发布文章
-                _this.saveArticleInfo(iframeData.param)
+                _this.saveArticleInfo(iframeData.article)
             }
         }
 
@@ -40,19 +42,59 @@ export default class mobileEditor extends React.Component {
      * 发布文章
      * @param param
      */
-    saveArticleInfo(param) {
+    saveArticleInfo(article) {
+        var imageList = [];
+        var attacheMents = article.attacheMents;
+        var newAttachMents = [];
+        for (var k in attacheMents) {
+            newAttachMents.push({
+                userId: _this.state.loginUser.uid,
+                type: attacheMents[k].type == 'image' ? 0 : 1,
+                path: attacheMents[k].path,
+                coverPath: attacheMents[k].cover,
+                isCover: attacheMents[k].isMainCover,
+                // articleId: _this.state.artId,
+                // attachmentId: attacheMents[k].attachmentId || -1,
+            })
+
+            if (attacheMents[k].isMainCover) {
+                imageList.push(attacheMents[k].cover);
+            }
+        }
+        this.saveArticleInfoFinal(article.title, article.content, article.author, article.type, imageList, newAttachMents);
+    }
+
+    saveArticleInfoFinal(title, html, author, type, imgArray, articleAttachments) {
+        var param = {
+            "method": 'saveArticleInfo',
+            "articleInfoJson": {
+                "userId": this.state.loginUser.uid,
+                "schoolId": this.state.loginUser.schoolId,
+                "gradeId": this.state.loginUser.gradeId,
+                "status": type,
+                "articleTitle": title,
+                "articleContent": html,
+                "author": author,
+                "articleAttachments": WebServiceUtil.isEmpty(articleAttachments) ? [] : articleAttachments
+            }
+        };
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: result => {
-                if (result.msg == '调用成功' || result.success) {
-                    if (param.articleInfoJson.status === 0) {
-                        Toast.success('保存成功', 1)
-                    } else {
-                        Toast.success('发布成功', 1)
-                    }
+                if (result.success) {
+                    Toast.info(type == 0 ? '保存成功' : '发布成功', 1);
+                    //关闭
+                    var data = {
+                        method: 'finish',
+                    };
+                    Bridge.callHandler(data, null, function (error) {
+                        console.log(error);
+                    });
+                } else {
+                    Toast.fail("保存/发布失败");
                 }
             },
             onError: function (error) {
-
+                Toast.fail(error, 1);
             }
         });
     }
@@ -65,13 +107,7 @@ export default class mobileEditor extends React.Component {
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: result => {
                 if (result.success) {
-
-                    var data = {}
-                    data.method = 'loginUserObj';
-                    data.mes = result.response
-
-                    var ifm = document.getElementById('iframe_mobile');
-                    ifm.contentWindow.postMessage(JSON.stringify(data), '*');
+                    _this.setState({loginUser: result.response})
                 }
             },
             onError: function (error) {

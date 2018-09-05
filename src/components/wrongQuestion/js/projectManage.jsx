fcbd862bf184387bac701a04f07344c977a11cab
zhaojuan <343524032@qq.com>
2018-09-06 06:50:31 +0800

    -

--- src/components/wrongQuestion/js/projectManage.jsx
+++ src/components/wrongQuestion/js/projectManage.jsx
@@ -353,7 +353,7 @@ export default class projectManage extends React.Component {
                                )
                            })
                        }
-                       <span className='spanTag' onClick={calm.addProject}>+添加科目</span>
+                       <span className='spanTag add' onClick={calm.addProject}>+添加科目</span>
                    </div>
                    <div className="allProject">
                            <div className='title'>所有科目

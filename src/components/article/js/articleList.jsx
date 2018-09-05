50650f001486d91a9212674371c08aadf1085b08
luble <282558417@qq.com>
2018-09-05 20:05:14 +0800

    -

--- src/components/article/js/articleList.jsx
+++ src/components/article/js/articleList.jsx
@@ -632,7 +632,7 @@ export default class articleList extends React.Component {
                         <div className="createTime"></div>
 
                     </div>
-                    <div className="tags">{rowData.type?'主题任务':'错题本'}</div>
+                    <div className="tags"><span className={rowData.type?"tag-ThemeTask":"tag-WrongTopic"}>{rowData.type?'':''}</span></div>
                     <div className="list_content">{rowData.content}</div>
                     <div className="list_image" style={
                         friendsAttachments.length == 0 ? {display: 'none'} : {display: 'block'}

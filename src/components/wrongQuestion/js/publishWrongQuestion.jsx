138443ac186d839c79e61bd660a03ed9de255e57
zhaojuan <343524032@qq.com>
2018-09-10 14:16:48 +0800

    -

--- src/components/wrongQuestion/js/publishWrongQuestion.jsx
+++ src/components/wrongQuestion/js/publishWrongQuestion.jsx
@@ -1229,7 +1229,7 @@ export default class publishWrongQuestion extends React.Component {
                                         )
                                     })
                                 }
-                                <span className='spanTag add' onClick={calm.addProject}>+添加科目</span>
+                                <span className='spanTag add' onClick={calm.addProject}>添加科目</span>
                             </div>
                             <div className="allProject">
                                 <div className='title'>所有科目

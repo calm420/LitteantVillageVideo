5e6418d16bb6f7d60fa0e20942d48a65989b82c8
calm <calm420@163.com>
2018-09-10 18:07:50 +0800

    .

--- src/components/wrongQuestion/js/publishWrongQuestion.jsx
+++ src/components/wrongQuestion/js/publishWrongQuestion.jsx
@@ -27,12 +27,12 @@ export default class publishWrongQuestion extends React.Component {
                 // }
             ],
             theQustionVideo: [
-                {
-                    type: 1,  //视频
-                    fatherType: 0,
-                    path: "http://60.205.86.217/upload8/2018-08-30/14/0e6f6e14-1a14-4f52-8096-431cd59ff6c3.mp4",
-                    coverPath: "http://60.205.86.217/upload8/2018-08-30/14/b02a7828-e89b-493e-a0ee-65a05b8f0da2.jpg"
-                }
+                // {
+                //     type: 1,  //视频
+                //     fatherType: 0,
+                //     path: "http://60.205.86.217/upload8/2018-08-30/14/0e6f6e14-1a14-4f52-8096-431cd59ff6c3.mp4",
+                //     coverPath: "http://60.205.86.217/upload8/2018-08-30/14/b02a7828-e89b-493e-a0ee-65a05b8f0da2.jpg"
+                // }
             ],
             theAnswerArr: [
                 // {

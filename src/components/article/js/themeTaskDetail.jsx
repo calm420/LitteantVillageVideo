bb79bfd299db6c410d5d5cd1a50d8673cffb9a89
luble <282558417@qq.com>
2018-09-17 18:27:51 +0800

    -

--- src/components/article/js/themeTaskDetail.jsx
+++ src/components/article/js/themeTaskDetail.jsx
@@ -794,7 +794,7 @@ export default class articleList extends React.Component {
                             scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
                             // useBodyScroll={true}
                             style={{
-                                height: document.body.clientHeight,
+                                height: document.body.clientHeight - 49,
                             }}
                         />
                     </div>
@@ -916,7 +916,7 @@ export default class articleList extends React.Component {
                             scrollEventThrottle={20}     //控制在滚动过程中，scroll事件被调用的频率
                             // useBodyScroll={true}
                             style={{
-                                height: document.body.clientHeight,
+                                height: document.body.clientHeight - 49,
                             }}
                         />
                     </div>
@@ -935,7 +935,7 @@ export default class articleList extends React.Component {
                     }>
                         <div>
                             <div className="import">
-                                <input
+                                <textarea
                                     type="text"
                                     id="commit"
                                     placeholder="请输入评论内容"

79379d50965f3e052163c3ae9b9436de0aa293fa
calm <calm420@163.com>
2018-10-23 17:44:39 +0800

    .

--- src/components/littleAntNoteSystem/rightVideoUpload/js/rightVideoUpload.jsx
+++ src/components/littleAntNoteSystem/rightVideoUpload/js/rightVideoUpload.jsx
@@ -121,7 +121,7 @@ export default class rightVideoUpload extends React.Component {
                     }>
                         <iframe id="iframe_box_add"
                                 src={"http://jiaoxue.maaee.com:8094/#/addUploadVideo?ident=" + this.state.userId}
-                                // src={"http://192.168.50.72:6094/#/addUploadVideo?ident=" + this.state.userId}
+                                // src={"http://192.168.50.72:7094/#/addUploadVideo?ident=" + this.state.userId}
                                 frameborder="0"></iframe>
                     </div>
                     <div style={
@@ -129,7 +129,7 @@ export default class rightVideoUpload extends React.Component {
                     }>
                         <iframe id="iframe_box_updata"
                                 src={"http://jiaoxue.maaee.com:8094/#/updateVideo?ident=" + this.state.userId + "&vId=" + this.state.vId}
-                                // src={"http://192.168.50.72:6094/#/updateVideo?ident=" + this.state.userId + "&vId=" + this.state.vId}
+                                // src={"http://192.168.50.72:7094/#/updateVideo?ident=" + this.state.userId + "&vId=" + this.state.vId}
                                 frameborder="0"></iframe>
                     </div>
                 </div>
